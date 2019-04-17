75fe65cbb40ce46cc325a2da08a7011a51b93380
Konstantin Tolstikhin <k.tolstikhin@gmail.com>
2019-04-17 13:07:30 +0800

    use mysql instead of sqlite; update backup script

--- cron/bin/backup.sh
+++ cron/bin/backup.sh
@@ -11,22 +11,13 @@ else
   exit 1
 fi
 
-if [ -z $DATA_PATH ]; then
-  echo "[ERROR] The DATA_PATH is unset!"
+archive=${BACKUP_PATH}/mysql_backup_${timestamp}.sql.gz
+echo "[INFO] Backup ${MYSQL_DATABASE} database to ${archive}"
+
+mysqldump -f -u root -p${MYSQL_ROOT_PASSWORD} -h ${MYSQL_HOST} ${MYSQL_DATABASE} | gzip > ${archive}
+
+if [ $? -ne 0 ]; then
+  echo "[ERROR] Backup failed!"
   exit 1
 fi
-
-for data_dir in $(ls -d ${DATA_PATH}/*_data); do
-  name=$(basename $data_dir)
-  archive=${DATA_PATH}/backup/${name}_backup_${timestamp}.tar.gz
-
-  echo "[INFO] Backup $data_dir to $archive"
-  tar -zcf $archive $data_dir
-
-  if [ $? -ne 0 ]; then
-    echo "[ERROR] Backup failed!"
-    exit 1
-  fi
-
-done
 
