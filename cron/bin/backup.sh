5c6af1d4d4cd3c5025fa134dc4cc81ae693f234b
Konstantin Tolstikhin <k.tolstikhin@gmail.com>
2019-04-18 21:28:00 +0800

    add date to backup logs

--- cron/bin/backup.sh
+++ cron/bin/backup.sh
@@ -2,27 +2,28 @@
 
 period=${1:-daily}
 
-if [ $period = daily ]; then
+if [ ${period} = daily ]; then
   timestamp=$(date +\%A)
-elif [ $period = weekly ]; then
+elif [ ${period} = weekly ]; then
   timestamp=$(date -I)
 else
-  echo "[ERROR] Unsupported backup period!"
+  echo "$(date) [ERROR] Unsupported backup period!"
   exit 1
 fi
 
 archive=${BACKUP_PATH}/mysql_backup_${timestamp}.sql.gz
-echo "[INFO] Backup ${MYSQL_DATABASE} database to ${archive}"
+echo "$(date) [INFO] Backup ${MYSQL_DATABASE} database to ${archive}"
 
 mysqldump \
-    -f \
-    -u root \
-    -p${MYSQL_ROOT_PASSWORD} \
-    -h ${MYSQL_HOST} ${MYSQL_DATABASE} \
-    | gzip > ${archive}
+  -f \
+  -u root \
+  -p${MYSQL_ROOT_PASSWORD} \
+  -h ${MYSQL_HOST} \
+  ${MYSQL_DATABASE} \
+  | gzip > ${archive}
 
 if [ $? -ne 0 ]; then
-  echo "[ERROR] Backup failed!"
+  echo "$(date) [ERROR] Backup failed!"
   exit 1
 fi
 
